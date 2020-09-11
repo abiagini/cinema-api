14b1a02180a4a723573032b056eceb57fcc0c109
Agustin Biagini <biaginiagustin@gmail.com>
2020-09-11 02:05:38 -0300

    modified movie title faker

--- database/factories/MovieFactory.php
+++ database/factories/MovieFactory.php
@@ -7,7 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Movie::class, function (Faker $faker) {
     return [
-        'title'       => $faker->sentence,
+        'title'       => $faker->sentence(3, true),
         'description' => $faker->paragraph,
         'rating'      => $faker->numberBetween(0, 5),
         'image_url'   => $faker->imageUrl(),
